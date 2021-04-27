76a7db7fbbf611373a803aea4e8143a1cf6a8da5
1gilmar <1gilmar@gmail.com>
2021-04-27 17:22:03 -0300

    criado rotina para definir o prefixo

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ const readLine = require('readline-sync')
 function start(){
   const content = {}
 
-  content.searchTerm = askAddReturnSearchTerm()
+  content.searchTerm = askAndReturnSearchTerm()
+  content.prefix = askAndReturnPrefix()
 
-  function askAddReturnSearchTerm(){
+  function askAndReturnSearchTerm(){
     return readLine.question('Digite termo de busca: ')
+  }
+
+  function askAndReturnPrefix(){
+    const prefixes = ['Who is', 'What is', 'The history of']
+    const selectedPrefixIndex = readLine.keyInSelect(prefixes, 'Selecion uma opção: ')
+    const selectedPrefixText = prefixes[selectedPrefixIndex];
+
+    return selectedPrefixText
   }
 
   console.log(content)
