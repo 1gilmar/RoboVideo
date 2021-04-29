0823873af27abf22f9d0840d94b9aa82c6093944
1gilmar <1gilmar@gmail.com>
2021-04-29 16:58:31 -0300

    ajustes

--- robots/input.js
+++ robots/input.js
@@ -2,35 +2,35 @@ const readline = require('readline-sync')
 const state = require('./state.js')
 
 function robot() {
-  const content = {
-    maximumSentences: 10
-  }
+	const content = {
+		maximumSentences: 10
+	}
 
-  content.searchTerm = askAndReturnSearchTerm()
-  content.prefix = askAndReturnPrefix()
-  content.lang = askLang()
+	content.searchTerm = askAndReturnSearchTerm()
+	content.prefix = askAndReturnPrefix()
+	content.lang = askLang()
 
-  console.log(content, 'vamos ver');
-  state.save(content)
+	// console.log(`> testar conteudo ${JSON.stringify(content)}`);
+	state.save(content)
 
-  function askAndReturnSearchTerm() {
-    return readline.question('Type a Wikipedia search term: ')
-  }
+	function askAndReturnSearchTerm() {
+		return readline.question('Type a Wikipedia search term: ')
+	}
 
-  function askAndReturnPrefix() {
-    const prefixes = ['Who is', 'What is', 'The history of']
-    const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
-    const selectedPrefixText = prefixes[selectedPrefixIndex]
+	function askAndReturnPrefix() {
+		const prefixes = ['Who is', 'What is', 'The history of']
+		const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
+		const selectedPrefixText = prefixes[selectedPrefixIndex]
 
-    return selectedPrefixText
-  }
+		return selectedPrefixText
+	}
 
-  function askLang(){
-    const lang = ['pt', 'en']
-    const lingua = readline.keyInSelect(lang,'Choose one language: ')
-    const linguaSelecionada = lang[lingua]
-    return linguaSelecionada
-  }
+	function askLang() {
+		const lang = ['pt', 'en']
+		const lingua = readline.keyInSelect(lang, 'Choose one language: ')
+		const linguaSelecionada = lang[lingua]
+		return linguaSelecionada
+	}
 
 }
 
