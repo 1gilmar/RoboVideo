322e59dd58222598ca705a239bc27835dfe2eb0b
1gilmar <1gilmar@gmail.com>
2021-04-29 18:00:17 -0300

    adicionando credencial google

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const robots = {
   // userInput: require('./robots/user-input.js'),
-  texto: require('./robots/text.js'),
-  input: require('./robots/input')
+  comandos: require('./robots/input'),
+  texto: require('./robots/text.js')
 };
 
 async function start() {
 
-  robots.input()
+  robots.comandos()
   await robots.texto()
   // await robots.image()
   // await robots.video()
